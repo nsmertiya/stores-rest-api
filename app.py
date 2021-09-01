bd8974f76ba7dc4edf033113be66806d6ffe02e3
nsmertiya <88333561+nsmertiya@users.noreply.github.com>
2021-09-01 16:22:36 +0530

    update to implement heroku postgres db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """MAIN APP for the REST Services of Items."""
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -11,16 +12,11 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "password"
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 # '/auth' redirects to the JWT Function
 jwt = JWT(app, authenticate, identity)
@@ -32,6 +28,5 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
